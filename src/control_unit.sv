8a98011ede2529f490d67fbd0b87d2e554e499a2
michel-uergs <117387840+michel-uergs@users.noreply.github.com>
2022-11-24 07:40:14 -0300

    Add files via upload
    control:
             - só com as instruções de buscar e decodificar

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -21,5 +21,47 @@ import k_and_s_pkg::*;
     output logic                    halt
 );
 
+typedef enum { 
+    DECODIFICA
+,   BUSCA_INSTR
+
+} STATE_T;
+
+STATE_T state;
+STATE_T next_state;
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rsn_t) begin
+        state <= BUSCA_INSTR; 
+    end
+    else
+        state <= next_state;
+end
+
+always_comb begin : calc_next_state
+    //VALORES DEFAULT
+    branch = 1'd0;
+    pc_enable = 1'd0;
+    ir_enable = 1'd0;
+    write_reg_enable = 1'd0;
+    addr_sel = 1'd0;
+    c_sel = 1'd0;
+    operation = 2'd00;
+    flags_reg_enable = 1'd0;
+    ram_write_enable = 1'd0;
+    halt = 1'd0;
+
+    case (state)
+        BUSCA_INSTR: begin    
+            next_state = DECODIFICA;
+            ir_enable = 1'b1;
+            pc_enable = 1'b1;
+        end
+
+        DECODIFICA: begin    
+            next_state = BUSCA_INSTR;
+        end
+    endcase
+end : calc_next_state
 
 endmodule : control_unit
