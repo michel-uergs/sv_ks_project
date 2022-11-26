637ad8e788295f7529cd82a94c2de1ad24a06191
RobertPerquimUergs <117539638+RobertPerquimUergs@users.noreply.github.com>
2022-11-26 03:35:49 -0300

    Update k_and_s_pkg.sv

--- src/k_and_s_pkg.sv
+++ src/k_and_s_pkg.sv
@@ -1,21 +1,21 @@
 package k_and_s_pkg;
   typedef enum  logic [4:0] {
-     I_NOP          = "00000"   
-,    I_LOAD        =  "00001"    
-,    I_STORE       =  "00010"      
-,    I_MOVE        =  "00011"    
-,    I_ADD         =  "00100"    
-,    I_SUB         =  "00101"    
-,    I_AND         =  "00110"    
-,    I_OR          =  "00111"  
-,    I_BRANCH      =  "01000"      
-,    I_BZERO       =  "01001" 
-,    I_BNZERO      =  "01010" 
-,    I_BNEG        =  "01011" 
-,    I_BNNEG       =  "01100" 
-,    I_BOV         =  "01101" 
-,    I_BNOV        =  "01110" 
-,    I_HALT        =  "11111" 
+     I_NOP          
+,    I_LOAD           
+,    I_STORE            
+,    I_MOVE           
+,    I_ADD           
+,    I_SUB           
+,    I_AND        
+,    I_OR          
+,    I_BRANCH         
+,    I_BZERO      
+,    I_BNZERO      
+,    I_BNEG        
+,    I_BNNEG       
+,    I_BOV         
+,    I_BNOV        
+,    I_HALT        
 } decoded_instruction_type;  // Decoded instruction in decode
 
 endpackage : k_and_s_pkg
